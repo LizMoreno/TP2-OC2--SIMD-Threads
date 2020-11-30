30109d4118058b9838980ceb025dceeda9458776
Yair Ruiz Barbás <yair.ruizbarbas@gmail.com>
2020-11-30 17:37:51 -0300

    Update script-c.sh

--- desarollo-lineal/script-c.sh
+++ desarollo-lineal/script-c.sh
@@ -1,3 +1,1 @@
-nasm -f elf32 enmascarar_asm.s -o enmascarar_asm.o;
-gcc -m32 -o enmascarar_asm enmascarar_asm.o enmascarar_asm.c;
-gcc enmascarar_c.c -o enmascarar_c;
+gcc  main_lineal  enmascara_c_lineal.c;
