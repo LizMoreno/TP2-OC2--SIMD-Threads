58a8764dbd11a8a0cb6e9eef1859e816b733ed46
Yair Ruiz Barbás <yair.ruizbarbas@gmail.com>
2020-11-30 17:41:04 -0300

    Update script.sh

--- desarrollo-hilos/script.sh
+++ desarrollo-hilos/script.sh
@@ -1,3 +1,2 @@
 nasm -f elf32 enmascarar_asm.s -o enmascarar_asm.o;
-gcc -m32 -o enmascarar_asm enmascarar_asm.o enmascarar_asm.c;
-gcc enmascarar_c.c -o enmascarar_c;
+gcc -m32 -pthreads -o main_hilos_asm enmascarar_asm.o enmascarar_hilos_asm.c;
